ec98ad0fbb24fd03f1e03f32e204a84cf90ac9b6
Kei Iwasaki <me@laughk.org>
2016-03-06 18:27:22 +0900

    不要ファイルの削除

--- roles/zabbix/files/initialize-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if (psql -c \\dt zabbix | grep -q No\ relations\ found) ; then
-  /bin/zcat /usr/share/doc/zabbix-server-pgsql/create.sql.gz > /tmp/initialized-zabbix.sql && \
-  psql -U zabbix zabbix < /tmp/initialized-zabbix.sql && \
-  rm -v /tmp/initialized-zabbix.sql
-fi
